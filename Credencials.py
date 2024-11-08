203c570e9706ae5524575f0ab704591fa91bfe34
RonNahmany <136971944+RonNahmany@users.noreply.github.com>
2024-11-08 16:09:16 +0200

    Update Credencials.py

--- Credencials.py
+++ Credencials.py
@@ -2,5 +2,5 @@ from enum import Enum
 
 
 class Cred(Enum):
-    Access = "AKIA5FTZBGWL6G3I7F3Y"
-    Secret = "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"
+    Access = "######"
+    Secret = "#####"
